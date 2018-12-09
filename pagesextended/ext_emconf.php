3ed3cbd7c2a6d3406c30d1429fbd8af0c5c855ac
Hendrik Legge <hendrik.legge@themepoint.de>
2018-12-09 22:35:47 +0100

    Added ext_emconf.php

---
+++ pagesextended/ext_emconf.php
@@ -0,0 +1,41 @@
+<?php
+/**
+ * Pages Extended
+ * Copyright (c) shopbase
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+$EM_CONF[$_EXTKEY] = array (
+    'title' => 'Pages Extended',
+    'description' => '',
+    'category' => 'be',
+    'shy' => 0,
+    'version' => '0.1',
+    'dependencies' => '',
+    'conflicts' => '',
+    'priority' => '',
+    'loadOrder' => '',
+    'module' => '',
+    'state' => 'beta',
+    'uploadfolder' => 0,
+    'createDirs' => '',
+    'modify_tables' => 'pages,pages_language_overlay',
+    'clearcacheonload' => 0,
+    'lockType' => '',
+    'author' => 'Shopbase',
+    'author_email' => 'info@themepoint.de',
+    'author_company' => 'ThemePoint',
+    'CGLcompliance' => NULL,
+    'CGLcompliance_note' => NULL,
+    'constraints' => 
+    array (
+        'depends' => array (
+            'typo3' => '6.2',
+        ),
+        'conflicts' => '',
+        'suggests' => array (
+        ),
+    ),
+);
